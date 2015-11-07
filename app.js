4d2d7ed5440d996c9c92696a7ba4427546fadf73
Ryan <ryankanno@localkinegrinds.com>
2015-11-06 19:56:54 -1000

    Added slash endpoint

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
+app.post('/slash', function(req, res) {
+});
+
 var port = 8080;
 app.listen(port);
 console.log('Express server started on port %s', port);
